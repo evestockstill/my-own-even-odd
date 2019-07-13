7f572bf9a70da9928de2e918fce3cb6ed2ec08c2
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-12 17:56:51 -0700

    final

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-function isEven(number) {
-    return number % 2 === 0;
+import isEven from './is-even.js';
+
+const submit = document.getElementById('submit');
+const input = document.getElementById('input');
+const evenPicture = document.getElementById('the-even');
+const oddPicture = document.getElementById('the-odd');
+
+submit.onclick = function() {
+    const inputValue = Number(input.value);
+  
+    if(isEven(inputValue)) {
+        evenPicture.classList.remove('hidden');
+        oddPicture.classList.add('hidden');
+    }
+    else {
+        oddPicture.classList.remove('hidden');
+        evenPicture.classList.add('hidden');
+      
+    }
+
 }
- 
-export default isEven;
-function isEven(number {
-    return (number) ! == 0;
-})
