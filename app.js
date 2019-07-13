2dccec7ca61568d7bc0e30ff71996af695fe035f
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-12 17:27:14 -0700

    round

---
+++ app.js
@@ -0,0 +1,8 @@
+function isEven(number) {
+    return number % 2 === 0;
+}
+ 
+export default isEven;
+function isEven(number {
+    return (number) ! == 0;
+})
--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,17 @@
+import isEven from '../is-even.js';
 const test = QUnit.test;
 
-test('time to test!', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
+test('is-even', function(assert) {
+   //Arrange
+   // Set up your parameters and expectations
+    const x = 2;
+    const expected = true;
 
-    //Act 
-    // Call the function you're testing and set the result to a const
+   //Act
+   // Call the function you’re testing and set the result to a const
+    const remainder = isEven(x);
 
-    //Assert
-    // What should the result be?
-    assert.equal(true, false);
+   //Assert
+   // What should the result be?
+    assert.equal(remainder, expected);
 });
